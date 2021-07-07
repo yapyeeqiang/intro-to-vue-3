3711d05172390d97cd9919f399ec715d500633da
Yap Yee Qiang <yapyeeqiang@gmail.com>
2021-07-08 05:18:16 +0800

    Extract product-related data to a ProductDisplay component, passing props from parent to child component

--- main.js
+++ main.js
@@ -2,48 +2,8 @@ const app = Vue.createApp({
 	data() {
 		return {
 			cart: 0,
-			product: "Socks",
-			brand: "Vue Mastery",
-			details: ["50% cotton", "30% wool", "20% polyester"],
-			selectedVariant: 0,
-			variants: [
-				{
-					id: 2234,
-					color: "green",
-					image: "./assets/images/socks_green.jpg",
-					quantity: 50,
-				},
-				{
-					id: 2235,
-					color: "blue",
-					image: "./assets/images/socks_blue.jpg",
-					quantity: 0,
-				},
-			],
-			onSale: false,
+			premium: true,
 		};
 	},
-	methods: {
-		addToCart() {
-			this.cart += 1;
-		},
-		updateVariant(index) {
-			this.selectedVariant = index;
-		},
-	},
-	computed: {
-		title() {
-			if (this.onSale) {
-				return `${this.brand} ${this.product} is on sale`;
-			} else {
-				return `${this.brand} ${this.product}`;
-			}
-		},
-		image() {
-			return this.variants[this.selectedVariant].image;
-		},
-		inStock() {
-			return this.variants[this.selectedVariant].quantity;
-		},
-	},
+	methods: {},
 });
