4b325b3efffe09c103577e5a028b8e3f80c8fb28
Yap Yee Qiang <yapyeeqiang@gmail.com>
2021-07-08 05:28:43 +0800

    Update the updateCart method, push the product id into the cart, display the length of the cart

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -6,41 +6,38 @@ app.component("product-display", {
 		},
 	},
 	template:
-		/* HTML */
+		/*html*/
 		`<div class="product-display">
-			<div class="product-container">
-				<div class="product-image">
-					<img v-bind:src="image" />
-				</div>
-				<div class="product-info">
-					<h1>{{ title }}</h1>
-
-					<p v-if="inStock">In Stock</p>
-					<p v-else>Out of Stock</p>
-					<p>Shipping: {{ shipping }}</p>
-					<ul>
-						<li v-for="detail in details">{{ detail }}</li>
-					</ul>
-
-					<div
-						v-for="(variant, index) in variants"
-						:key="variant.id"
-						@mouseover="updateVariant(index)"
-						class="color-circle"
-						:style="{ backgroundColor: variant.color }"
-					></div>
-
-					<button
-						class="button"
-						:class="{ disabledButton: !inStock }"
-						:disabled="!inStock"
-						@click="addToCart"
-					>
-						Add to Cart
-					</button>
-				</div>
-			</div>
-		</div>`,
+             <div class="product-container">
+                <div class="product-image">
+                    <img v-bind:src="image">
+                </div>
+                <div class="product-info">
+                    <h1>{{ title }}</h1>
+                    <p v-if="inStock">In Stock</p>
+                    <p v-else>Out of Stock</p>
+                    <p>Shipping: {{ shipping }}</p>
+                    <ul>
+                        <li v-for="detail in details">{{ detail }}</li>
+                    </ul>
+                    <div 
+                        v-for="(variant, index) in variants" 
+                        :key="variant.id" 
+                        @mouseover="updateVariant(index)" 
+                        class="color-circle" 
+                        :style="{ backgroundColor: variant.color }">
+                    </div>
+            
+                    <button 
+                        class="button" 
+                        :class="{ disabledButton: !inStock }" 
+                        :disabled="!inStock" 
+                        @click="addToCart">
+                        Add to Cart
+                    </button>
+                </div>
+            </div>
+        </div>`,
 	data() {
 		return {
 			product: "Socks",
@@ -65,7 +62,7 @@ app.component("product-display", {
 	},
 	methods: {
 		addToCart() {
-			this.cart += 1;
+			this.$emit("add-to-cart", this.variants[this.selectedVariant].id);
 		},
 		updateVariant(index) {
 			this.selectedVariant = index;
@@ -84,9 +81,8 @@ app.component("product-display", {
 		shipping() {
 			if (this.premium) {
 				return "Free";
-			} else {
-				return 2.99;
 			}
+			return 2.99;
 		},
 	},
 });
