2e0757a5be6183348cad837fd1b33ba1b060703b
Yap Yee Qiang <yapyeeqiang@gmail.com>
2021-07-07 16:58:30 +0800

    Added buttons to allow the product to be added to cart or removed from cart, and also added a mouseover listener to update the image when user hover over the specific color of the socks

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
 const app = Vue.createApp({
 	data() {
 		return {
+			cart: 0,
 			product: "Socks",
 			image: "./assets/images/socks_blue.jpg",
 			inStock: true,
 			details: ["50% cotton", "30% wool", "20% polyester"],
 			variants: [
-				{ id: 2234, color: "green" },
-				{ id: 2235, color: "blue" },
-			],
-			sizes: [
-				{ id: "S", type: "Small" },
-				{ id: "M", type: "Medium" },
-				{ id: "L", type: "Large" },
-				{ id: "XL", type: "Extra large" },
+				{ id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
+				{ id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
 			],
 		};
+	},
+	methods: {
+		addToCart() {
+			this.cart += 1;
+		},
+		removeFromCart() {
+			this.cart -= 1;
+		},
+		updateImage(variantImage) {
+			this.image = variantImage;
+		},
 	},
 });
